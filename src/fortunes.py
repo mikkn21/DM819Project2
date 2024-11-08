1c3b484ab517517612b9b8279d3d2184d82cc333
Jonas Bork <jonas10bork@gmail.com>
2024-11-08 14:22:02 +0100

    aoeu

--- src/fortunes.py
+++ src/fortunes.py
@@ -1,4 +1,3 @@
-
 import math
 from point import Point
 from events import *
@@ -8,42 +7,50 @@ import numpy as np
 from tree import check_circle_event
 from find_breakpoint import define_circle
 
+
 def fortunes(points: list[Point]) -> Edge:
     event_queue = EventQueue([SiteEvent(p) for p in points])
-     
-    status = Tree(None, event_queue)
-    dcel: Edge = None 
 
-    def delete_circle_event_if_using(using_leaf, leaf) -> None:
+    status = Tree(None, event_queue)
+    dcel: Edge = None
+
+    def delete_circle_event_if_using(using_leaf: Leaf, leaf: Leaf) -> None:
         """
         Deletes the circle event on leaf from the event_queue if the using_leaf is a part of the circle event
         Except if the using_leaf is in the middle :)
         """
-        if leaf.circle_event is not None and (id(leaf.circle_event.left_arc) == id(using_leaf) or id(leaf.circle_event.right_arc) == id(using_leaf)):
+        if leaf == None:
+            return
+        if leaf.circle_event is not None and (
+            id(leaf.circle_event.left_arc) == id(using_leaf)
+            or id(leaf.circle_event.right_arc) == id(using_leaf)
+        ):
             print("Deleting a circle event because it's using the leaf")
             event_queue.remove(leaf.circle_event)
             leaf.circle_event = None
 
     def handle_circle_event(event: CircleEvent) -> None:
         nonlocal dcel
-        # Step 1 
-        # status.remove(event.middle_leaf) 
+        # Step 1
+        # status.remove(event.middle_leaf)
         # TODO: Assuming that the removed element is replaced all the way up the tree with the left-most leaf.
         #       Also, assume that it's always the middle arc that is removed by a circle event, i.e., it always has a leaf to the left and to the right of it.
-        #       Check with Kim if this is correct 
-        
+        #       Check with Kim if this is correct
+
         print("Handle circle event. Middle leaf: ", event.middle_leaf.site)
-        
+
         # Get neighbour leaves
-        p_next = event.middle_leaf.next_leaf()     
+        p_next = event.middle_leaf.next_leaf()
         p_next_next = p_next.next_leaf()
         p_prev = event.middle_leaf.prev_leaf()
         p_prev_prev = p_prev.prev_leaf()
         parent = event.middle_leaf.parent
         grand_parent = parent.parent
         print("")
-        print(f"Parent: {parent.arc_points[0], parent.arc_points[1], parent.edge.origin}, Grand parent: {grand_parent.arc_points[0], grand_parent.arc_points[1], grand_parent.edge.origin}")
-        print(" ") 
+        print(
+            f"Parent: {parent.arc_points[0], parent.arc_points[1], parent.edge.origin}, Grand parent: {grand_parent.arc_points[0], grand_parent.arc_points[1], grand_parent.edge.origin}"
+        )
+        print(" ")
         # Update the tuples representing the breakpoints at the internal nodes
         # TODO: Can we assume we can do this in all cases?
         #       If it works, it might be because we don't balance the tree, and we always create a subtree in the format below.
@@ -52,7 +59,7 @@ def fortunes(points: list[Point]) -> Edge:
         # p_prev.parent = grand_parent
         grand_parent.left = parent.left
         parent.left.parent = grand_parent
-    
+
         # Remove other circle events that use the arc
         delete_circle_event_if_using(event.middle_leaf, p_next)
         delete_circle_event_if_using(event.middle_leaf, p_next_next)
@@ -60,23 +67,29 @@ def fortunes(points: list[Point]) -> Edge:
         delete_circle_event_if_using(event.middle_leaf, p_prev_prev)
 
         # Step 2
-        center_of_circle, r = define_circle(p_prev.site, event.middle_leaf.site, p_next.site)
-        if center_of_circle is None: # TODO: REMOVE 
+        center_of_circle, r = define_circle(
+            p_prev.site, event.middle_leaf.site, p_next.site
+        )
+        if center_of_circle is None:  # TODO: REMOVE
             raise ValueError("Circle center is None in handle_circle_event")
-        vertex = Vertex(center_of_circle.x, center_of_circle.y, None) # TODO: Set edge reference (if needed)
-        
+        vertex = Vertex(
+            center_of_circle.x, center_of_circle.y, None
+        )  # TODO: Set edge reference (if needed)
+
         print("")
         print(f"Center of circle: {center_of_circle.x, center_of_circle.y}, r: {r}")
         print(" ")
 
         vertex.edge = Edge(vertex, None, None, None, None)
 
-  
-
-        new_edge = Edge(Point(vertex.x, vertex.y), vertex.edge, None, None, None) # The edge of the new breakpoint
+        new_edge = Edge(
+            Point(vertex.x, vertex.y), vertex.edge, None, None, None
+        )  # The edge of the new breakpoint
         vertex.edge.twin = new_edge
         # TODO: check if all the pointers are set correctly
-        if id(event.middle_leaf) == id(parent.right): # Parent is the breakpoint coming from the left
+        if id(event.middle_leaf) == id(
+            parent.right
+        ):  # Parent is the breakpoint coming from the left
             print("Parent is coming from left")
             print("Parent edge origin: ", parent.edge.origin)
             print("Parent twin edge origin: ", parent.edge.twin.origin)
@@ -88,7 +101,7 @@ def fortunes(points: list[Point]) -> Edge:
             grand_parent.edge.origin = vertex
             # grand_parent.edge.twin.set_next(vertex.edge)
             vertex.edge.set_prev(grand_parent.edge.twin)
-        else: # Parent is the breakpoint coming from the right
+        else:  # Parent is the breakpoint coming from the right
             print("Parent is coming from right")
             new_edge.set_next(grand_parent.edge)
             grand_parent.edge.twin.set_next(parent.edge)
@@ -102,16 +115,13 @@ def fortunes(points: list[Point]) -> Edge:
         print("Grand parent twin edge origin: ", grand_parent.edge.twin.origin)
         grand_parent.edge = new_edge
 
-
-        # TODO: Make less hacky 
+        # TODO: Make less hacky
         if dcel is None:
             dcel = vertex.edge
 
         # Step 3
         check_circle_event(p_prev_prev, p_prev, p_next, event.key, event_queue)
         check_circle_event(p_prev, p_next, p_next_next, event.key, event_queue)
-
-
 
     while not event_queue.is_empty():
         event = event_queue.pop()
@@ -127,9 +137,7 @@ def fortunes(points: list[Point]) -> Edge:
             print("After circle event:")
             status.print_tree()
     # TODO: Step 7
-    
-    
-    # TODO: Step 8 (Skip for now)
 
+    # TODO: Step 8 (Skip for now)
 
     return dcel
--- src/visualization/dcel_plot.py
+++ src/visualization/dcel_plot.py
@@ -1,80 +1,26 @@
-
 import sys
 import matplotlib.pyplot as plt
 from dcel import *
 from point import Point
 
-def plt_draw_new(edge: Edge, depth: int, target_depth: int) -> None:
-    print(edge)
-    if depth >= target_depth:
-        return
-    
-    # Draw the vertex
-    plt.plot(edge.origin.x, edge.origin.y, 'ko') 
-
-    # Draw the edge
-    point_to = edge.next.origin if edge.next != None else edge.twin.origin
-    color = "black" if edge.next != None else "red"
-    plt.annotate("",
-                xytext=(edge.origin.x, edge.origin.y), xy=(point_to.x, point_to.y),
-                arrowprops=dict(arrowstyle="->", lw=1.5, color=color))
-    
-    # Draw twin
-    if edge.twin != None:
-        plt_draw_new(edge.twin, depth + 1, target_depth)
-    
-    # Draw next edge
-    if edge.next != None:
-        plt_draw_new(edge.next, depth + 1, target_depth)
-
-    # Draw prev edge
-    if edge.prev != None:
-        plt_draw_new(edge.prev, depth + 1, target_depth)
-
-def plt_draw(edge: Edge, drawn_edges: set[Edge], drawn_vertices: set[Vertex]) -> None:
-    print(edge)
-    if edge in drawn_edges:
-        return
-    
-    drawn_edges.add(edge)
-    
-    # Draw the vertex
-    if edge.origin not in drawn_vertices:
-        plt.plot(edge.origin.x, edge.origin.y, 'ko') 
-        drawn_vertices.add(edge.origin)
-
-    # Draw the edge
-    point_to = edge.next.origin if edge.next != None else edge.twin.origin
-    color = "black" if edge.next != None else "red"
-    plt.annotate("",
-                xytext=(edge.origin.x, edge.origin.y), xy=(point_to.x, point_to.y),
-                arrowprops=dict(arrowstyle="->", lw=1.5, color=color))
-    
-    # Draw twin
-    if edge.twin != None:
-        plt_draw(edge.twin, drawn_edges, drawn_vertices)
-    
-    # Draw next edge
-    if edge.next != None:
-        plt_draw(edge.next, drawn_edges, drawn_vertices)
-
-    # Draw prev edge
-    if edge.prev != None:
-        plt_draw(edge.prev, drawn_edges, drawn_vertices)
 
 def plot_edges(edges: set[Edge]) -> None:
     for edge in edges:
         print("plt edge")
         point_to = edge.next.origin if edge.next != None else edge.twin.origin
-        color = "black" if edge.next != None else "red"
-        plt.annotate("",
-                    xytext=(edge.origin.x, edge.origin.y), xy=(point_to.x, point_to.y),
-                    arrowprops=dict(arrowstyle="->", lw=1.5, color=color))
+        color = "black"
+        plt.annotate(
+            "",
+            xytext=(edge.origin.x, edge.origin.y),
+            xy=(point_to.x, point_to.y),
+            arrowprops=dict(arrowstyle="->", lw=1.5, color=color),
+        )
+
 
 def plot_vertices(vertices: set[Vertex]) -> None:
     for vertex in vertices:
         print("plt vertex")
-        plt.plot(vertex.x, vertex.y, 'ko') 
+        plt.plot(vertex.x, vertex.y, "bo" if isinstance(vertex, Point) else "ko")
 
 
 def dcel_plot(init_edge: Edge, sites: list[Point]) -> None:
@@ -86,7 +32,7 @@ def dcel_plot(init_edge: Edge, sites: list[Point]) -> None:
     plot_vertices(vertices)
 
     for site in sites:
-        plt.plot(site.x, site.y, 'ro')
+        plt.plot(site.x, site.y, "ro")
 
     print()
     print("Drawing DCEL...")
@@ -102,24 +48,26 @@ def dcel_plot(init_edge: Edge, sites: list[Point]) -> None:
     print("Vertices:")
     for vertex in vertices:
         print(vertex)
-    
-    coordinates = [(v.x, v.y) for v in vertices] + [(v.x, v.y) for v in sites]    
+
+    coordinates = [(v.x, v.y) for v in vertices] + [(v.x, v.y) for v in sites]
     max_x = max([v[0] for v in coordinates])
     min_x = min([v[0] for v in coordinates])
     max_y = max([v[1] for v in coordinates])
     min_y = min([v[1] for v in coordinates])
     padding = 5
+    max_y = 250
+    max_x = 150
 
-    plt.xlim(min_x - padding, max_x + padding) 
+    plt.xlim(min_x - padding, max_x + padding)
     plt.ylim(min_y - padding, max_y + padding)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Result')
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Result")
     plt.grid()
-    plt.axhline(0, color='black', linewidth=0.5, ls='--')
-    plt.axvline(0, color='black', linewidth=0.5, ls='--')
-    
-    plt.savefig("result.pdf", format='pdf')
+    plt.axhline(0, color="black", linewidth=0.5, ls="--")
+    plt.axvline(0, color="black", linewidth=0.5, ls="--")
+
+    plt.savefig("result.pdf", format="pdf")
     plt.close()
 
 
@@ -140,8 +88,10 @@ def get_all_edges(edge: Edge, visited: set[Edge] = None) -> list[Edge]:
         edges.extend(get_all_edges(edge.prev, visited))
     return edges
 
+
 def get_all_vertices(edges: list[Edge]) -> set[Vertex]:
     vertices = set()
     for edge in edges:
         vertices.add(edge.origin)
     return vertices
+
