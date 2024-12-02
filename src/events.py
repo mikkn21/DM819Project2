8b7480d15fac5fa2be82fdbc61c14bbdd2ce4580
Jonas Bork <jonas10bork@gmail.com>
2024-12-02 19:26:19 +0100

    Define circle event by its sites, rather than middle leaf

--- src/events.py
+++ src/events.py
@@ -1,26 +1,42 @@
-
 from __future__ import annotations
 from dataclasses import dataclass
 from sortedcontainers import SortedListWithKey
 from point import Point
 
+
 @dataclass
 class Event:
     key: float
+
 
 class SiteEvent(Event):
     def __init__(self, site: Point):
         super().__init__(site.y)
         self.site = site
 
+
 class CircleEvent(Event):
-    def __init__(self, middle_leaf, circle_bottom_y: float): # middle_leaf is a Leaf but to avoid circular dependency, it cannot be type hinted
+    def __init__(
+        self, sites: list[Point], circle_bottom_y: float
+    ):  # middle_leaf is a Leaf but to avoid circular dependency, it cannot be type hinted
         super().__init__(circle_bottom_y)
-        self.middle_leaf = middle_leaf
+        if len(sites) != 3:
+            raise Exception("Circle events need 3 sites but were not given 3")
+        self.sites = sites
+    
+    def __eq__(self, other: CircleEvent) -> bool:
+        for i in range(len(self.sites)):
+            if self.sites[i] != other.sites[i]:
+                return False
+        return True
+    
+    def __hash__(self) -> int:
+        return hash(tuple(self.sites))
+
 
 class EventQueue:
     def __init__(self, events: list[SiteEvent | CircleEvent] = []):
-        self.events = SortedListWithKey(events, key = lambda event: event.key)
+        self.events = SortedListWithKey(events, key=lambda event: event.key)
         self.size = len(events)
 
     def add(self, event: SiteEvent | CircleEvent) -> None:
@@ -40,5 +56,3 @@ class EventQueue:
 
     def is_empty(self) -> bool:
         return self.size <= 0
-        
-
