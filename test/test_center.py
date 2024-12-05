0b2f3b9541be6cd42e21f50539407afe7a096167
Jonas Bork <jonas10bork@gmail.com>
2024-12-05 12:22:16 +0100

    Fixed tests

--- test/test_center.py
+++ test/test_center.py
@@ -1,20 +1,20 @@
-
 import math
 
 import numpy as np
-from find_breakpoint import define_circle
+from geometry import define_circle
 from point import Point
+
 
 def test_center_on_circle():
 
-    p1 = Point(4,4)
+    p1 = Point(4, 4)
     p2 = Point(5, 5)
     p3 = Point(7, 4)
-    p, r = define_circle(p1, p2, p3) 
+    p, r = define_circle(p1, p2, p3)
     assert p == Point(5.5, 3.5), f"Center of circle is not as expected p = {p}"
     assert math.isclose(r, 1.5811388300841898), f"Radius is not as expected r = {r}"
-    
-    
+
+
 def test_center_not_on_circle():
     p1 = Point(11, 2)
     p2 = Point(13, 2)
@@ -22,3 +22,4 @@ def test_center_not_on_circle():
     p, r = define_circle(p1, p2, p3)
     assert p == None, f"Center of circle is not as expected p = {p}"
     assert r == np.inf, f"Radius is not as expected r = {r}"
+
